c32d5bd00c80e71de4fe406a28956265f11ed75f
endymion1818 <endymion1818@gmail.com>
2021-05-19 17:46:49 +0100

    fix for gallery layout

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -80,54 +80,54 @@
 </h1>
 <main class="markdown">
   {@html page.content.html}
+  {#if (page.slug === 'gallery')}
     <section class="container mx-auto mb-8">
-      {#if (page.slug === 'gallery')}
-      <div class="grid md:grid-flow-col gap-2">
-        {#each galleries as gallery}
-          <div
-            class="p-12 flex flex-col items-start w-full border rounded-xl shadow-xl bg-white"
-          >
-            <h2
-              class="sm:text-3xl text-2xl title-font font-medium mt-4 mb-4"
-            >
-              {gallery.title}
-            </h2>
-            <p class="leading-relaxed mb-8">
-              {gallery.description}
-            </p>
-            <a href={`/gallery/${gallery.slug}`} class="inline-flex items-center">
-              <img
-                alt={`Preview image for gallery ${gallery.title}`}
-                src={gallery.featuredImage.url}
-                width={gallery.featuredImage.width}
-                height={gallery.featuredImage.height}
-                class="h-120 flex-shrink-0 object-cover object-center"
-              />
-            </a>
-            <div
-              class="flex items-center flex-wrap mb-4 mt-auto w-full"
-            >
-              <a
-                href={`/gallery/${gallery.slug}`}
-                class="text-indigo-500 inline-flex items-center mt-2"
-                >Learn More
-                <svg
-                  class="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+          <div class="grid md:grid-cols-2 gap-2">
+            {#each galleries as gallery}
+              <div
+                class="p-12 flex flex-col items-start w-full border rounded-xl shadow-xl bg-white"
+              >
+                <h2
+                  class="sm:text-3xl text-2xl title-font font-medium mt-4 mb-4"
                 >
-                  <path d="M5 12h14" />
-                  <path d="M12 5l7 7-7 7" />
-                </svg>
-              </a>
-            </div>
-          </div>
-        {/each}
-      </div>
+                  {gallery.title}
+                </h2>
+                <p class="leading-relaxed mb-8">
+                  {gallery.description}
+                </p>
+                <a href={`/gallery/${gallery.slug}`} class="inline-flex items-center">
+                  <img
+                    alt={`Preview image for gallery ${gallery.title}`}
+                    src={gallery.featuredImage.url}
+                    width={gallery.featuredImage.width}
+                    height={gallery.featuredImage.height}
+                    class="h-120 flex-shrink-0 object-cover object-center"
+                  />
+                </a>
+                <div
+                  class="flex items-center flex-wrap mb-4 mt-auto w-full"
+                >
+                  <a
+                    href={`/gallery/${gallery.slug}`}
+                    class="text-indigo-500 inline-flex items-center mt-2"
+                    >Learn More
+                    <svg
+                      class="w-4 h-4 ml-2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M5 12h14" />
+                      <path d="M12 5l7 7-7 7" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+          {/each}
+        </div>
+      </section>
       {/if}
-    </section>
 </main>
